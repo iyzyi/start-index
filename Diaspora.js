026870af8b3865cc552f604fd82bfdfcedf9f465
iyzyi <kljxn@qq.com>
2020-02-19 09:44:34 +0800

    删除diaspora.js一些无关代码，暂时没发现bug，先打个包暂存一下

--- Diaspora.js
+++ Diaspora.js
@@ -82,7 +82,6 @@ var Diaspora = {
                     setTimeout(function() { Diaspora.player(state.d) }, 0)
                 })
             }
-
         })
     },
 
@@ -131,71 +130,9 @@ var Diaspora = {
                     Diaspora.loaded()
                 break;
             }
-
-            setTimeout(function() {
-                if (!id) id = $('.icon-play').data('id');
-                Diaspora.player(id)
-
-                // get download link
-                $('.content img').each(function() {
-                    if ($(this).attr('src').indexOf('/uploads/2014/downloading.png') > -1) {
-                        $(this).hide()
-                        $('.downloadlink').attr('href', $(this).parent().attr('href'))
-                    }
-                })
-
-                if (flag == 'replace') {
-                    $('#top').show()
-                }
-            }, 0)
-
         })
     },
 
-    preview: function() {
-        setTimeout(function() {
-            $('#preview').addClass('show')
-            $('#container').data('scroll', window.scrollY)
-            setTimeout(function() {
-                $('#container').hide()
-                setTimeout(function() {
-                    $('#preview').css({
-                        'position': 'static',
-                        'overflow-y': 'auto'
-                    }).removeClass('trans')
-                    $('#top').show()
-
-                    Diaspora.loaded()
-                }, 500)
-            }, 300)
-        }, 0)
-    },
-
-    player: function(id) {
-        var p = $('#audio-'+ id +'-1');
-
-        if (!p.length) {
-            $('.icon-play').css({
-                'color': '#dedede',
-                'cursor': 'not-allowed'
-            })
-            return
-        }
-
-        p[0].play()
-
-        p.on({
-            'timeupdate': function() {
-                $('.bar').css('width', p[0].currentTime / p[0].duration * 100 +'%')
-            },
-            'ended': function() {
-                $('.icon-pause').removeClass('icon-pause').addClass('icon-play')
-            },
-            'playing': function() {
-                $('.icon-play').removeClass('icon-play').addClass('icon-pause')
-            } 
-        })
-    },
 
     loading: function() {
         var w = window.innerWidth;
@@ -305,7 +242,6 @@ $(function() {
             if (swatches['Vibrant']) {
                 $('.icon-menu').css('color', swatches['Vibrant'].getHex())
             }
-
         })
 
         if (!cover.t.attr('src')) {
@@ -315,8 +251,6 @@ $(function() {
         $('#preview').css('min-height', window.innerHeight)
 
         Diaspora.PS()
-
-        $('.pview a').addClass('pviewa')
 
         var T;
         $(window).on('resize', function() {
@@ -333,56 +267,15 @@ $(function() {
                 }
             }, 500)
         })  
-
     } else {
 
-        $('#single').css('min-height', window.innerHeight)
         $('html, body').removeClass('loading')
         
         window.addEventListener('popstate', function(e) {
             if (e.state) location.href = e.state.u;
         })
-
-        Diaspora.player($('.icon-play').data('id'))
-
-        $('.icon-icon, .image-icon').attr('href', '/')
-
-        // get download link
-        $('.content img').each(function() {
-            if ($(this).attr('src').indexOf('/uploads/2014/downloading.png') > -1) {
-                $(this).hide()
-                $('.downloadlink').attr('href', $(this).parent().attr('href')).css('display', 'block')
-            }
-        })
-
-        $('#top').show()
-
     }
 
-    $(window).on('scroll', function() {
-        if ($('.scrollbar').length && !Diaspora.P() && !$('.icon-images').hasClass('active')) {
-            var st = $(window).scrollTop(),
-                ct = $('.content').height();
-
-            if (st > ct) {
-                st = ct
-            }
-
-            $('.scrollbar').width((50 + st) / ct * 100 +'%')
-
-            if (st > 80 && window.innerWidth > 800) {
-                $('.subtitle').fadeIn()
-            } else {
-                $('.subtitle').fadeOut()
-            }
-        }
-    })
-
-    $(window).on('touchmove', function(e) {
-        if ($('body').hasClass('mu')) {
-            e.preventDefault()
-        }
-    })
 
     $('body').on('click', function(e) {
 
@@ -390,217 +283,5 @@ $(function() {
             rel = $(e.target).attr('rel') || '';
 
         if (!tag && !rel) return;
-
-        switch (true) {
-
-            // nav menu
-            case (tag.indexOf('switchmenu') != -1):
-                window.scrollTo(0, 0)
-                $('html, body').toggleClass('mu')
-            break;
-
-            // next page
-            case (tag.indexOf('more') != -1):
-                tag = $('.more');
-
-                if (tag.data('status') == 'loading') {
-                    return false
-                }
-
-                var num = parseInt(tag.data('page')) || 1;
-
-                if (num == 1) {
-                    tag.data('page', 1)
-                }
-
-                if (num >= Pages) {
-                    return
-                }
-
-                tag.html('加载中..').data('status', 'loading')
-                Diaspora.loading()
-
-                Diaspora.L(tag.attr('href'), function(data) {
-                    var link = $(data).find('.more').attr('href');
-                    if (link != undefined) {
-                        tag.attr('href', link).html('加载更多').data('status', 'loaded')
-                        tag.data('page', parseInt(tag.data('page')) + 1)
-                    } else {
-                        $('#pager').remove()
-                    }
-                    
-                    var tempScrollTop = $(window).scrollTop();
-                    $('#primary').append($(data).find('.post'))
-                    $(window).scrollTop(tempScrollTop);
-                    Diaspora.loaded()
-                    $('html,body').animate({ scrollTop: tempScrollTop + 400 }, 500);
-                }, function() {
-                    tag.html('加载更多').data('status', 'loaded')
-                })
-
-                return false;
-            break;
-
-            // post images
-            case (tag.indexOf('icon-images') != -1):
-                window.scrollTo(0, 0)
-
-                var d = $('.icon-images');
-
-                if (d.data('status') == 'loading') {
-                    return false
-                }
-
-                if (d.hasClass('active')) {
-                    d.removeClass('active')
-
-                    $('.article').css('height', 'auto')
-                    $('.section').css('left', '-100%')
-                    setTimeout(function() {
-                        $('.images').data('height', $('.images').height()).css('height', '0') 
-                    }, 0)
-                } else {
-                    d.addClass('active')
-
-                    $('.images').css('height', $('.images').data('height'))
-
-                    if ($('.icon-images').hasClass('tg')) {
-                        $('.section').css('left', 0)
-
-                        setTimeout(function() { $('.article').css('height', '0') }, 0)
-                    } else {
-                        if (!(Diaspora.P() && window.innerWidth < 700)) {
-                            $('.zoom').Chocolat()
-                        }
-
-                        Diaspora.loading()
-                        d.data('status', 'loading')
-
-                        var m = 5, r = 120;
-                        if (Diaspora.P() && window.innerWidth < 600) {
-                            m = 1;
-                            r = 80;
-                        }
-                        $('#jg').justifiedGallery({
-                            margins: m, 
-                            rowHeight : r,
-                        }).on('jg.complete', function () {
-                            $('.section').css('left', 0)
-                            $('.icon-images').addClass('tg')
-
-                            d.data('status', '')
-                            Diaspora.loaded()
-                            setTimeout(function() { $('.article').css('height', '0') }, 0)
-                        })
-                    }
-
-                }
-            break;
-
-            // qrcode
-            case (tag.indexOf('icon-wechat') != -1):
-                if ($('.icon-wechat').hasClass('tg')) {
-                    $('#qr').toggle()
-                } else {
-                    $('.icon-wechat').addClass('tg')
-                    $('#qr').qrcode({ width: 128, height: 128, text: location.href}).toggle()
-                }
-            break;
-
-            // audio play
-            case (tag.indexOf('icon-play') != -1):
-                $('#audio-'+ $('.icon-play').data('id') +'-1')[0].play()
-                $('.icon-play').removeClass('icon-play').addClass('icon-pause')
-            break;
-
-            // audio pause
-            case (tag.indexOf('icon-pause') != -1):
-                $('#audio-'+ $('.icon-pause').data('id') +'-1')[0].pause()
-                $('.icon-pause').removeClass('icon-pause').addClass('icon-play')
-            break;
-
-            // post like
-            case (tag.indexOf('icon-like') != -1):
-                var t = $(e.target).parent(),
-                    classes = t.attr('class');
-
-                if (t.prev().hasClass('icon-view')) return;
-
-                classes = classes.split(' ');
-                if(classes[1] == 'active') return;
-
-                t.addClass('active')
-
-                var id = t.attr('id').split('like-');
-
-                $.ajax({
-                    type: 'POST',
-                    url: '/index.php',
-                    data: 'likepost=' + id[1],
-                    success: function() {
-                        var text = $('#like-'+ id[1]).html(),
-                            patt= /(\d)+/,
-                            num = patt.exec(text);
-
-                        num[0] ++;
-                        $('#like-'+ id[1]).html('<span class="icon-like"></span><span class="count">' + num[0] + '</span>')
-                    }
-                })
-            break;
-
-            // history state
-            case (tag.indexOf('cover') != -1):
-                Diaspora.HS($(e.target).parent(), 'push')
-                return false;
-            break;
-
-            // history state
-            case (tag.indexOf('posttitle') != -1):
-                Diaspora.HS($(e.target), 'push')
-                return false;
-            break;
-
-            // relate post
-            case (tag.indexOf('relatea') != -1):
-                Diaspora.HS($(e.target), 'replace')
-                return false;
-            break;
-
-            // relate post
-            case (tag.indexOf('relateimg') != -1):
-                Diaspora.HS($(e.target).parent(), 'replace')
-                return false;
-            break;
-
-            // prev, next post
-            case (rel == 'prev' || rel == 'next'):
-                if (rel == 'prev') {
-                    var t = $('#prev_next a')[0].text
-                } else {
-                    var t = $('#prev_next a')[1].text
-                }
-                $(e.target).attr('title', t)
-
-                Diaspora.HS($(e.target), 'replace')
-                return false;
-            break;
-
-            // quick view
-            case (tag.indexOf('pviewa') != -1):
-                $('body').removeClass('mu')
-
-                setTimeout(function() {
-                    Diaspora.HS($(e.target), 'push')
-                }, 300)
-
-                return false;
-            break;
-
-            default:
-                return;
-            break;
-        }
-
     })
-
 })
